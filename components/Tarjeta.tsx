8b76c2a1f36b1c92321755d8c9a290136035e04c
lucasherediam <lherediamartinelli@gmail.com>
2024-09-03 16:48:15 -0300

    Added styles and logic for onPress

--- components/Tarjeta.tsx
+++ components/Tarjeta.tsx
@@ -1,38 +1,55 @@
-import { View, Text, Button, Pressable, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
 type Props = {
-    nombre: string;
-  }
+  nombre: string;
+};
 
 const styles = StyleSheet.create({
-    onPressStyles: {
-        backgroundColor: 'blue'
-    },
-    unpressedStyles: {
-        height: 100,
-        width: 300,
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 10,
-        backgroundColor: 'blue',
-    },
-    textStyles : {
-        textAlign: 'center'
-    }
-})
+  onPressStyles: {
+    backgroundColor: 'yellow',
+    height: 100,
+    width: 300,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  unpressedStyles: {
+    backgroundColor: 'darkblue',
+    height: 100,
+    width: 300,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  textStyles: {
+    textAlign: 'center',
+    color: 'white', 
+  },
+  textOnPressStyles: {
+    textAlign: 'center',
+    color: 'darkblue',
+  },
+});
 
 const Tarjeta = (props: Props) => {
+  const [isPressed, setIsPressed] = useState(false);
 
-    // const [nombre, setContador] = useState(0)
-    return (
-        <>
-            <Pressable style={styles.unpressedStyles}>
-                <Text style={styles.textStyles}>{props.nombre}</Text>
-            </Pressable>
-        </>
-    )
-}
+  return (
+    <>
+      <Pressable
+        onPressIn={() => setIsPressed(true)}
+        onPressOut={() => setIsPressed(false)}
+        style={isPressed ? styles.onPressStyles : styles.unpressedStyles}
+      >
+        <Text style={isPressed ? styles.textOnPressStyles : styles.textStyles}>
+          {props.nombre}
+        </Text>
+      </Pressable>
+    </>
+  );
+};
 
-export default Tarjeta
+export default Tarjeta;
