7717609af0d59a4489e598eb7fddcb2110928fa0
lucasherediam <lherediamartinelli@gmail.com>
2024-09-03 16:03:34 -0300

    Added styles to Tarjeta.tsx

--- components/Tarjeta.tsx
+++ components/Tarjeta.tsx
@@ -1,17 +1,35 @@
-import { View, Text, Button, Pressable } from 'react-native'
+import { View, Text, Button, Pressable, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 
 type Props = {
     nombre: string;
   }
 
+const styles = StyleSheet.create({
+    onPressStyles: {
+        backgroundColor: 'blue'
+    },
+    unpressedStyles: {
+        height: 100,
+        width: 300,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        backgroundColor: 'blue',
+    },
+    textStyles : {
+        textAlign: 'center'
+    }
+})
+
 const Tarjeta = (props: Props) => {
 
     // const [nombre, setContador] = useState(0)
     return (
         <>
-            <Pressable>
-                <Text>{props.nombre}</Text>
+            <Pressable style={styles.unpressedStyles}>
+                <Text style={styles.textStyles}>{props.nombre}</Text>
             </Pressable>
         </>
     )
